6f5be44307822e3fc382352f67efcf41e4a1c1be
El-Hassan Wanas <wanas@leastauthority.com>
2021-09-14 11:38:25 +0300

    Try calling dart port sender directly without wormhole_william interaction

--- lib/src/async_callback.c
+++ lib/src/async_callback.c
@@ -4,9 +4,14 @@
 #include <dart_api.h>
 #include <dart_api_dl.h>
 
-// extern GoInt ClientSendText(void* ctxC, GoUintptr clientPtr, char* msgC, char** codeOutC, callback cb);
+// extern GoInt ClientSendText(void* ctxC, GoUintptr clientPtr, char* msgC,
+// char** codeOutC, callback cb);
 
 // int main() {}
+//
+intptr_t init_dart_api_dl(void* data) {
+  return Dart_InitializeApiDL(data);
+}
 
 typedef struct {
   intptr_t callback_port;
@@ -25,10 +30,14 @@ void async(void *ctx, void *value, int32_t err_code) {
   }
 
   intptr_t callback_port = ((context *)(ctx))->callback_port;
+
+  Dart_CObject* obj = malloc(sizeof(Dart_CObject));
+  obj->type = Dart_CObject_kString;
+  obj->value.as_string = "done from C";
+  // *obj = {.type = Dart_CObject_kString, .value.as_string = "I'm done yo"};
+
   // Send dart object response.
-  bool result = Dart_PostCObject_DL(
-      callback_port, &(((Dart_CObject){.type = Dart_CObject_kString,
-                                       .value.as_string = "I'm done yo"})));
+  bool result = Dart_PostCObject_DL(callback_port, obj);
 }
 
 int client_send_text(uintptr_t clientPtr, char *msg, char **_codeOut,
@@ -36,6 +45,8 @@ int client_send_text(uintptr_t clientPtr, char *msg, char **_codeOut,
   context ctx = {.callback_port = callback_port};
 
   *_codeOut = "hello";
+
+  // async(&ctx, NULL, 0);
 
   return 0;
 
