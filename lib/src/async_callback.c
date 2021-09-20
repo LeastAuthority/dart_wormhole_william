06a5e61a5fdf7f05cb33ccf202414e431046c75b
Bryan White <bryanchriswhite@gmail.com>
2021-09-20 12:50:13 +0200

    wip

--- lib/src/async_callback.c
+++ lib/src/async_callback.c
@@ -30,22 +30,24 @@ void async_callback(void *ctx, void *value, int32_t err_code) {
     printf("Sending callback result to dart isolate failed");
   }
 
-// TODO: why does this cause a double free error?
-  //free(ctx);
+  free(ctx);
 }
 
 int async_ClientSendText(uintptr_t client_id, char *msg, char **code_out, int32_t callback_port_id) {
-  context ctx = {.callback_port_id = callback_port_id};
-  return ClientSendText(&ctx, client_id, msg, code_out, async_callback);
+  context *ctx = (context *)(malloc(sizeof(context)));
+  *ctx = (context){.callback_port_id = callback_port_id};
+  return ClientSendText(ctx, client_id, msg, code_out, async_callback);
 }
 
 // TODO: factor `file_name`, `lenght`, and `file_bytes` out to a struct.
 int async_ClientSendFile(uintptr_t client_id, char *file_name, int32_t length, void *file_bytes, char **code_out, int32_t callback_port_id) {
-  context ctx = {.callback_port_id = callback_port_id};
-  return ClientSendFile(&ctx, client_id, file_name, length, file_bytes, code_out, async_callback);
+  context *ctx = (context *)(malloc(sizeof(context)));
+  *ctx = (context){.callback_port_id = callback_port_id};
+   return ClientSendFile(ctx, client_id, file_name, length, file_bytes, code_out, async_callback);
 }
 
 int async_ClientRecvText(uintptr_t client_id, char *code, int32_t callback_port_id) {
-  context ctx = {.callback_port_id = callback_port_id};
-  return ClientRecvText(&ctx, client_id, code, async_callback);
+  context *ctx = (context *)(malloc(sizeof(context)));
+  *ctx = (context){.callback_port_id = callback_port_id};
+  return ClientRecvText(ctx, client_id, code, async_callback);
 }
