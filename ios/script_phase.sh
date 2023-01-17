4a3334cdf2588f0d169b661c64c5d4637be71604
Donatas Puidokas <104410044+donpui@users.noreply.github.com>
2023-01-17 08:51:55 +0200

    Changes to make it work again

--- ios/script_phase.sh
+++ ios/script_phase.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env bash -x
+#!/bin/bash -x
+
+# Note: For xcode to build, might need to set env
+#export PATH=$PATH:/opt/homebrew/bin &&
+#export FLUTTER_ROOT=/Users/donataspuidokas/Apps/flutter &&
 
 cd $TARGET_BUILD_DIR
 
 mkdir -p $TARGET_BUILD_DIR/Headers
 
+# Note: For xcode build might need to set full path to cmake
 cmake \
   -GXcode \
   -DCMAKE_OSX_SYSROOT=$SDK_DIR \
@@ -18,5 +23,5 @@ xcodebuild -alltargets -configuration $CONFIGURATION
 
 cp -v \
   $TARGET_BUILD_DIR/$CONFIGURATION-$PLATFORM_NAME/{libbindings.a,libdart_wormhole_william_plugin.a} \
-  $TARGET_BUILD_DIR/$PLATFORM_PREFERRED_ARCH/libwormhole_william.{h,a} \
-  $PODS_CONFIGURATION_BUILD_DIR
+  $TARGET_BUILD_DIR/libwormhole_william.{h,a} \
+  $PODS_CONFIGURATION_BUILD_DIR/
